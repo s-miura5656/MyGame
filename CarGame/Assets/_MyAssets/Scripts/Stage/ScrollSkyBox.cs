9374eb3884fe5f8a8b544002e2195482f0632ab3
souta_miura <miura042910@gmail.com>
2021-01-28 01:36:07 +0900

    feat:Skyboxのスクロール機能追加

---
+++ CarGame/Assets/_MyAssets/Scripts/Stage/ScrollSkyBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class ScrollSkyBox : MonoBehaviour
+{
+    [SerializeField] private Material _skybox = null;
+    [SerializeField] private float scrollSpeed = 0.001f;
+    private float _rotation = 0f;
+
+    void Start()
+    {
+        Observable.EveryUpdate()
+            .TakeUntilDestroy(this)
+            .Subscribe(scroll => {
+                _skybox.SetFloat("_Rotation", AddRotationValue());
+            });
+    }
+
+    private float AddRotationValue() 
+    {
+        if (_rotation < 360)
+            _rotation += scrollSpeed;
+        else
+            _rotation = 0f;
+
+        return _rotation;
+    }
+}
